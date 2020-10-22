d10dd0cd8ad4dc83a3c203f53d809e13e593b626
kavehkm <mehrbaniankaveh@gmail.com>
2020-10-22 10:59:00 +0330

    add reset_time() method to ChessPlayer class, also replace QThread with QTimer approach and finally adding reset method to ChessClockModel

--- src/Model.py
+++ src/Model.py
@@ -1,25 +1,27 @@
-# standard
-from time import sleep
 # external
-from PyQt5.QtCore import QObject, pyqtSignal, QThread
+from PyQt5.QtCore import QObject, QTimer, pyqtSignal
 
 
 class ChessPlayer(object):
     """Chess Player"""
     def __init__(self, _time):
         self._time = _time
+        self._dec = 0
+        self._inc = 0
 
     def get_time(self):
-        return self._time
+        return self._time + self._inc - self._dec
 
     def inc_time(self):
-        self._time += 1
+        self._inc += 1
 
     def dec_time(self):
-        if self._time - 1 < 0:
-            self._time = 0
-        else:
-            self._time -= 1
+        if self.get_time() - 1 >= 0:
+            self._dec += 1
+
+    def reset_time(self):
+        self._dec = 0
+        self._inc = 0
 
 
 class ChessClockModelSignals(QObject):
@@ -27,26 +29,13 @@ class ChessClockModelSignals(QObject):
     change = pyqtSignal(int, int)
 
 
-class CThread(QThread):
-    """Chess Thread"""
-    def __init__(self, func, *args, **kwargs):
-        self._func = func
-        self._args = args
-        self._kwargs = kwargs
-        super().__init__()
-
-    def run(self):
-        self._func(*self._args, **self._kwargs)
-
-
 class ChessClockModel(object):
     """Chess Clock Model"""
     def __init__(self, white_time, black_time):
-        self._white_time = white_time
-        self._black_time = black_time
-        self._players = {1: ChessPlayer(self._white_time), -1: ChessPlayer(self._black_time)}
+        self._players = {1: ChessPlayer(white_time), -1: ChessPlayer(black_time)}
         self._turn = 1
-        self._thread = None
+        self._timer = QTimer()
+        self._timer.timeout.connect(self._waiting)
         self.signals = ChessClockModelSignals()
 
     def get_players_time(self):
@@ -56,21 +45,23 @@ class ChessClockModel(object):
         w, b = self.get_players_time()
         return w and b
 
+    def _stop_timer(self):
+        if self._timer.isActive():
+            self._timer.stop()
+
     def _waiting(self):
-        while self._players[self._turn].get_time() > 0:
-            sleep(1)
-            self._players[self._turn].dec_time()
-            self.signals.change.emit(*self.get_players_time())
+        self._players[self._turn].dec_time()
+        self.signals.change.emit(*self.get_players_time())
 
     def move(self):
-        if self._thread is not None and self._thread.isRunning():
-            self._thread.terminate()
+        self._stop_timer()
         if not self._game_is_on():
             return
         self._turn *= -1
-        self._thread = CThread(self._waiting)
-        self._thread.start()
+        self._timer.start(1000)
 
     def restart(self):
-        # must define
-        pass
+        self._stop_timer()
+        self._turn = 1
+        for player in self._players.values():
+            player.reset_time()
